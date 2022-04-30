db5899cb60b29dea6a9e5d4546409d196cbf230e
unknown <j.tamayoo@uniandes.edu.co>
2022-04-29 19:42:02 -0500

    Se crea el servicio para obtener el conjunto de datos de plantas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlantaModule } from './planta/planta.module';
@@ -12,7 +12,8 @@ import { PlantaModule } from './planta/planta.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PlantaModule
+    PlantaModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
