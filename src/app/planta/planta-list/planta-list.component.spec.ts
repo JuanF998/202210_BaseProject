f23e13ac9fe52ed76c54910cb1e6a3d24793592c
unknown <j.tamayoo@uniandes.edu.co>
2022-04-29 22:13:58 -0500

    Se agrega la prueba del componente listar plantas

--- src/app/planta/planta-list/planta-list.component.spec.ts
+++ src/app/planta/planta-list/planta-list.component.spec.ts
@@ -2,15 +2,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
+
 
 import { PlantaListComponent } from './planta-list.component';
+import { Planta } from '../planta';
 
 describe('PlantaListComponent', () => {
   let component: PlantaListComponent;
   let fixture: ComponentFixture<PlantaListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantaListComponent ]
     })
     .compileComponents();
@@ -19,7 +25,21 @@ describe('PlantaListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantaListComponent);
     component = fixture.componentInstance;
+    component.plantas = [
+      new Planta(faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence(), faker.lorem.sentence(), faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence()),
+      new Planta(faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence(), faker.lorem.sentence(), faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence()),
+      new Planta(faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence(), faker.lorem.sentence(), faker.datatype.number(), faker.lorem.sentence(), faker.lorem.sentence())
+    ]
     fixture.detectChanges();
+    debug = fixture.debugElement;
+  });
+
+  it("Component has a table with header", () => {
+    expect(debug.query(By.css("thead")).children.length).toEqual(1);
+  });
+
+  it("Component has a table with three rows of elements", () => {
+    expect(debug.query(By.css("tbody")).children.length).toEqual(component.plantas.length);
   });
 
   it('should create', () => {
